bb6301d36f1d222ed02068c4ea1ea431c3828a9e
janudaethmin <januda.b@insighture.com>
2025-04-29 15:18:39 +0530

    marshal

--- marshelling/main.go
+++ marshelling/main.go
@@ -1,20 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
 
 	user := User{
-		Name:     "John Doe",
-		Age:      30,
+		Name:     "Januda Bethmin",
+		Age:      24,
 		Password: "securepassword",
 	}
 	fmt.Println("User:", user)
+
+	userConvertedTOByteString, error := json.Marshal(user)
+
+	if error != nil {
+		fmt.Println("Error converting user to JSON:", error)
+		return
+	}
+
+	userConvertedTOJson := string(userConvertedTOByteString)
+	fmt.Println("User in JSON format:", userConvertedTOJson)
 }
 
 type User struct {
-	Name     string
-	Age      int
-	Password string
+	Name     string `json:"name,omitempty"`
+	Age      int    `json:"age"`
+	Password string `json:"password"`
 }
