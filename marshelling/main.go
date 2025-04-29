26f7ca6f5aaca423fc138dc3a936f224b2fafe25
janudaethmin <januda.b@insighture.com>
2025-04-29 16:06:36 +0530

    unmarshelling

--- marshelling/main.go
+++ marshelling/main.go
@@ -8,22 +8,37 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 
-	user := User{
-		Name:     "Januda Bethmin",
+	user1 := User{
+		Name:     "Januda",
 		Age:      24,
 		Password: "securepassword",
 	}
-	fmt.Println("User:", user)
+	fmt.Println("User1 :", user1)
 
-	userConvertedTOByteString, error := json.Marshal(user)
+	// Marshalling the user struct to JSON
+	userConvertedTOByteString, error1 := json.Marshal(user1)
 
-	if error != nil {
-		fmt.Println("Error converting user to JSON:", error)
+	if error1 != nil {
+		fmt.Println("Error converting user1 to JSON:", error1)
 		return
 	}
 
 	userConvertedTOJson := string(userConvertedTOByteString)
-	fmt.Println("User in JSON format:", userConvertedTOJson)
+	fmt.Println("User1 in JSON format:", userConvertedTOJson)
+
+	// Unmarshalling the JSON to a struct
+	jsonUser := `{"name":"Bethmin","age":24,"password":"mysecurepassword"}`
+
+	user2 := User{}
+
+	error2 := json.Unmarshal([]byte(jsonUser), &user2)
+
+	if error2 != nil {
+		fmt.Println("Error converting JSON to user2:", error2)
+		return
+	}
+
+	fmt.Println("User2 :", user2)
 }
 
 type User struct {
