8cc21a190a05ecc3961176b469139cd180ad211e
janudaethmin <januda.b@insighture.com>
2025-04-29 14:11:21 +0530

    struct user

--- marshelling/main.go
+++ marshelling/main.go
@@ -4,4 +4,17 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	user := User{
+		Name:     "John Doe",
+		Age:      30,
+		Password: "securepassword",
+	}
+	fmt.Println("User:", user)
+}
+
+type User struct {
+	Name     string
+	Age      int
+	Password string
 }
