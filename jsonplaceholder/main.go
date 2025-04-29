b44667badd7d5a7d8c2218bbfc759b9e7efc7f11
janudaethmin <januda.b@insighture.com>
2025-04-29 18:40:44 +0530

    http get

--- jsonplaceholder/main.go
+++ jsonplaceholder/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	url := "https://jsonplaceholder.typicode.com/posts"
+
+	response, error := http.Get(url)
+
+	if error != nil {
+		fmt.Println("Error fetching data:", error)
+		return
+	}
+
+	// Ensure the response body is closed after all operations
+	defer response.Body.Close()
+
+	// Check if the response status code is 200 OK
+	if response.StatusCode != http.StatusOK {
+		fmt.Println("Error: Status code", response.StatusCode)
+		return
+	}
+
+	// Get the pointer to the response body
+	responseBodyPointer := response.Body
+
+	// Use io.ReadAll to read the response body
+	responseBodyByteType, errorBody := io.ReadAll(responseBodyPointer)
+
+	if errorBody != nil {
+		fmt.Println("Error reading response body:", errorBody)
+		return
+	}
+
+	responseBody := string(responseBodyByteType)
+
+	fmt.Println("Response Body:", responseBody)
 }
