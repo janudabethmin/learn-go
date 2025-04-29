55acd5ab0ac12cee7592b15e055b331f1cfde4ad
janudaethmin <januda.b@insighture.com>
2025-04-29 16:58:37 +0530

    omiting and removing

--- marshalling/main.go
+++ marshalling/main.go
@@ -56,9 +56,37 @@ func main() {
 		Password: "mysecurepassword",
 	}
 
+	user3ConvertedTOByteString, error3 := json.Marshal(user3)
+
+	if error3 != nil {
+		fmt.Println("Error converting user3 to JSON:", error3)
+		return
+	}
+
+	user3ConvertedTOJson := string(user3ConvertedTOByteString)
+
+	fmt.Println("User3 in JSON format:", user3ConvertedTOJson)
 	fmt.Println("User3 :", user3)
 
 	// Removing a field from the JSON output
+
+	user4 := UserWhenPasswordIsNotSharedViaJson{
+		Name:     "Januda",
+		Age:      24,
+		Password: "mysecurepassword",
+	}
+
+	user4ConvertedTOByteString, error4 := json.Marshal(user4)
+
+	if error4 != nil {
+		fmt.Println("Error converting user4 to JSON:", error4)
+		return
+	}
+
+	user4ConvertedTOJson := string(user4ConvertedTOByteString)
+
+	fmt.Println("User4 in JSON format:", user4ConvertedTOJson)
+	fmt.Println("User4 :", user4)
 
 }
 
@@ -69,14 +97,14 @@ type User struct {
 }
 
 type UserWithOmitEmpty struct {
-	// omitempty omits the field from the JSON output if it is empty
+	// omitempty omits the field from the JSON output if it is empty as well as the field from the struct
 	Name     string `json:"name,omitempty"`
 	Age      int    `json:"age"`
 	Password string `json:"password"`
 }
 
 type UserWhenPasswordIsNotSharedViaJson struct {
-	// "-" omits the field from the JSON output no matter what
+	// "-" omits the field from the JSON output no matter what but keeps it in the struct
 	Name     string `json:"name"`
 	Age      int    `json:"age"`
 	Password string `json:"-"`
