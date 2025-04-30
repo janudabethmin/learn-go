4bfd6171cb43b8cc79ff88d245a4facfd38250b4
janudaethmin <januda.b@insighture.com>
2025-04-30 14:20:25 +0530

    fetch by id

--- crudGin/main.go
+++ crudGin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,7 +13,8 @@ func main() {
 
 	// Route a specific route to a function
 	router.GET("/books", getAllBooks)
-	router.POST("/books", addBook)
+	router.POST("/books", createBook)
+	router.GET("/books/:id", getBookById)
 
 	// Start the server on localhost:8080
 	router.Run("localhost:8080")
@@ -25,15 +27,15 @@ func getAllBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
-func addBook(c *gin.Context) {
+func createBook(c *gin.Context) {
 
 	// Create a new variable to hold the new book data
 	var newBook Book
 
-	// If the JSON is invalid, return a 400 Bad Request
 	error := c.BindJSON(&newBook)
 	if error != nil {
 		// gin.H is used to create a json object without creating a struct
+		// If the JSON is invalid, return a 400 Bad Request
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid input"})
 		return
 	}
@@ -43,6 +45,36 @@ func addBook(c *gin.Context) {
 
 	// Return the newly created book with a 201 Created status
 	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func getBookById(c *gin.Context) {
+
+	// Get the book ID from the URL
+	id := c.Param("id")
+
+	// Call the helper function to get the book by ID
+	book, err := bookById(id)
+	if err != nil {
+		// If the book is not found, return a 404 Not Found
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+	// Return the book as nicely indented JSON
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+// This is a helper function to get a book by its ID
+func bookById(id string) (*Book, error) {
+
+	// Loop through the books to find the book with the given ID
+	for i, book := range books {
+		if book.ID == id {
+			return &books[i], nil
+		}
+	}
+
+	// Uses the errors package to create a new error
+	return nil, errors.New("Book not found")
 }
 
 type Book struct {
