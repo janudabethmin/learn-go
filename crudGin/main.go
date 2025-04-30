45d13444df6df1ea0786f64773d42ccdc50fa695
janudaethmin <januda.b@insighture.com>
2025-04-30 13:46:59 +0530

    gin basic

---
+++ crudGin/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Initialize the Gin router
+	router := gin.Default()
+
+	// Route a specific route to a function
+	router.GET("/books", getAllBooks)
+	router.POST("/books", addBook)
+
+	// Start the server on localhost:8080
+	router.Run("localhost:8080")
+}
+
+// *gin.Context is a pointer to the context of the request that contains all the information about the request allowing us to create the response
+func getAllBooks(c *gin.Context) {
+
+	// Return the list of books as nicely indented JSON
+	c.IndentedJSON(http.StatusOK, books)
+}
+
+func addBook(c *gin.Context) {
+
+	// Create a new variable to hold the new book data
+	var newBook Book
+
+	// If the JSON is invalid, return a 400 Bad Request
+	error := c.BindJSON(&newBook)
+	if error != nil {
+		// gin.H is used to create a json object without creating a struct
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid input"})
+		return
+	}
+
+	// Add the new book to the in-memory database by
+	books = append(books, newBook)
+
+	// Return the newly created book with a 201 Created status
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+type Book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+// In-memory database of books
+var books = []Book{
+	{ID: "1", Title: "Book One", Author: "Author A", Quantity: 5},
+	{ID: "2", Title: "Book Two", Author: "Author B", Quantity: 3},
+	{ID: "3", Title: "Book Three", Author: "Author C", Quantity: 2},
+}
