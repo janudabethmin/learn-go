a42d9199f9bf5ffdf6484ea6856c603b17c45e15
itzzjb <desilvabethmin@gmail.com>
2025-05-03 05:05:08 +0530

    connect to db

--- jsoncrudGinGorm/initializers/connectDatabase.go
+++ jsoncrudGinGorm/initializers/connectDatabase.go
@@ -1,6 +1,27 @@
 package initializers
 
-// ConnectDatabase Connecting to the postgres database
-func ConnectDatabase() {
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
 
+// DB Database Connection Global Variable
+var DB *gorm.DB
+
+func init() {
+	// No need initializers.LoadEnvVariables because it's in the same package
+	LoadEnvVariables()
+}
+
+// ConnectDatabase Connecting to the PostgreSQL database
+func ConnectDatabase() {
+	var err error
+	dsn := os.Getenv("DB_STRING")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+		return
+	}
 }
