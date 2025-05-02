0838bfe802c827c429d2fe44a0344a7bd53b0d9e
itzzjb <desilvabethmin@gmail.com>
2025-05-02 19:05:01 +0530

    hello world server

--- jsoncrudGinGorm/main.go
+++ jsoncrudGinGorm/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("/hello", hello)
+	router.Run("localhost:8080")
+}
+
+func hello(c *gin.Context) {
+	c.IndentedJSON(200, gin.H{"message": "Hello World"})
 }
